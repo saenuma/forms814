ce655290ea1553c393c36a7684bb589da97157fe
Bankole Ojo <ojobankole@gmail.com>
2025-04-14 10:43:11 +0100

    added float type

--- defines.go
+++ defines.go
@@ -56,7 +56,7 @@ var (
 
 	cursorEventsCount = 0
 
-	supportedFields = []string{"int", "string", "text", "email", "date",
+	supportedFields = []string{"int", "string", "text", "email", "date", "float",
 		"datetime", "select", "multi_display_select", "single_display_select", "check"}
 	attributes = []string{"required", "unique", "nindex", "hidden"}
 
